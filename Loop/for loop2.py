79afe23cfebca93e6d6582cc39837f6a3de30685
Ruchi Lamichhane <ruchilamichhane2468@gmail.com>
2022-09-02 00:22:13 +0545

    update

--- Loop/for loop2.py
+++ Loop/for loop2.py
@@ -1,4 +1,4 @@
-#sum of all even number from 0 to 20
+'''#sum of all even number from 0 to 20
 sum=0
 for i in range (0,21):
   if i%2==0 :
@@ -17,9 +17,36 @@ n=int(input("Enter a number:"))
 for i in range (1,n+1) :
   for j in range(1,i) :
     print(j,end='')
-  print()
+  print()'''
 
-  
+
+  #using nested for loop acessing the elements of a matrix
+  #matrix as a list containing list
+  #take 2 such matrix from user and get sum
+
+r = int(input("Enter number of rows : "))
+c = int(input("Enter number of columns : "))
+X=[]
+val=[]
+for i in range(0,r):
+    for j in range(0, c):
+      val.insert(j,int(input("Enter the %d * %d element : " %(i,j))))
+    X.insert(i,val)
+    val=[]
+Y=[]
+for i in range(0, r):
+    for j in range(0, c):
+      val.insert(j,int(input("Enter the %d * %d element : " % (i, j))))
+    Y.insert(i, val)
+    val=[]
+sum=[]
+
+for i in range(0, r):
+  for j in range(0, c):
+    val.insert(j, X[i][j]+Y[i][j])
+    sum.insert(i,val)
+    val=[]
+print(sum)
 
 
 
