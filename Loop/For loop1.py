0ce7125af842a473b32eb396dc84e4f9df65818a
Ruchi Lamichhane <ruchilamichhane2468@gmail.com>
2022-09-01 23:45:33 +0545

    modified

--- Loop/For loop1.py
+++ Loop/For loop1.py
@@ -14,10 +14,12 @@ for i in X:
 for i in range(0, 6):
  print(i)
   
-  '''
 
+  
 for i in range(0, 21, 2):
   print(i)
+  '''
+
 
 
  
--- Loop/for loop2.py
+++ Loop/for loop2.py
@@ -1,14 +1,3 @@
-'''x=[1,4.4,"simplilearn"]
-for i in x :
-  print(i, end="") '''
-
-  
-"""for i in range(0,21) :
- print(i)
-
- for i in range (0,21,2) :
-  print(i) """
-
 sum=0
 for i in range (0,21):
   if i%2==0 :
